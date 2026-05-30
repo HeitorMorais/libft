16873afd825b46509c2d8aa54cdc6b8c9116ab71
HeitorMorais <heitormorais2003@gmail.com>
2026-05-30 00:45:21 -0300

    split

--- ft_split.c
+++ ft_split.c
@@ -1,32 +1,45 @@
 #include "libft.h"
-#include <stdio.h>
 char	**ft_split(char const *s, char c){
 	char **arr;
-	char	**ptr;
+	char **ptr_arr;
+	char *ptr;
 	int	i;
-
 	arr = malloc(ft_strlen(s) + 1);
-	ptr = arr;
+	ptr = malloc(ft_strlen(s) + 1);
+	ptr_arr = arr;
 	i = 0;
 	if(!arr) return NULL;
+	*ptr = '\0';
+	while(*s == c) s++;
 	while(*s){
-		if(*s != c){
-			ptr++;
+		if(*s == c){
+			while(*s == c) s++;
+			ptr[i] = '\0';
+			*ptr_arr = ft_strdup(ptr);
+			*ptr = '\0';
 			i = 0;
+			ptr_arr++;
 		} else {
-			*ptr[i] = *s;
+			ptr[i] = *s;
 			i++;
+			s++;
 		}
-		s++;
 	}
-	printf("%s", arr[1]);
-	*ptr = "\0";
+	if(ptr){
+		*ptr_arr = ft_strdup(ptr);
+		ptr_arr++;
+	}
+	*ptr_arr = "\0";
 	return arr;
 }
 
 int main(){
 	#include <stdio.h>
-	char **arr = ft_split("ab,cd,ef", ',');
-	//for(int i=0; i < 3; i++) printf("%s\n", arr[i]);
+	char const *str = "ab,cd,ef,gh";
+	char **arr = ft_split(str, ',');
+	while(*arr){
+		printf("%s\n", *arr);
+		arr++;
+	}
 	return 0;
 }
